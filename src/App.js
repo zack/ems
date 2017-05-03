be90292dcc4d0b827cde329fe09d452c8216bb97
Zack Youngren <zack@youngren.io>
2017-05-02 23:48:23 -0400

    Add a timer

--- src/App.js
+++ src/App.js
@@ -13,17 +13,20 @@ class App extends Component {
     const phrasographs = this.state.phrases.map((phrase) => {
       const initials = getInitials(phrase.words);
       return <Phrasograph description={phrase.desc}
-                          initials={initials}
-                          key={initials}
-                          word_arrays={phrase.words}
-             />;
+      initials={initials}
+      key={initials}
+      word_arrays={phrase.words}
+        />;
     });
 
-    return (
-      <div className="App">
+    const clock = <Clock />
+
+      return (
+        <div className="App">
+        {clock}
         {phrasographs}
-      </div>
-    );
+        </div>
+      );
   }
 }
 
@@ -32,15 +35,15 @@ class Phrasograph extends Component {
     const lines = this.props.word_arrays.map((word_array) => {
       const first_word = word_array[0];
       return <WordLine words={word_array}
-                       letter={first_word[0]}
-                       key={`${this.props.initials}_${first_word}`}
+      letter={first_word[0]}
+      key={`${this.props.initials}_${first_word}`}
         />;
     });
 
     return(
       <div className="phrasograph">
-        <div className="title"> {this.props.description} </div>
-        {lines}
+      <div className="title"> {this.props.description} </div>
+      {lines}
       </div>
     );
   }
@@ -69,8 +72,46 @@ class WordLine extends Component {
 
     return(
       <div className="line">
-        <div className="letter uppercase inline-block"> {this.props.letter} </div>
-        <input className={`inline-block ${statusClass}`} onChange={this.changeHandler.bind(this)}></input>
+      <div className="letter uppercase inline-block"> {this.props.letter} </div>
+      <input className={`inline-block ${statusClass}`} onChange={this.changeHandler.bind(this)}></input>
+      </div>
+    );
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      this.tick.bind(this),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      seconds: this.state.seconds + 1,
+    });
+  }
+
+  render() {
+    const seconds = ('00' + this.state.seconds % 60).slice(-2);
+    const minutes = ('00' + Math.floor(this.state.seconds / 60)).slice(-2);
+
+    return(
+      <div className="timer-container">
+        <div className="timer">{`${minutes}:${seconds}`}</div>
       </div>
     );
   }
